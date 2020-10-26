3d49bc1d71cb8711b10b5e763d1b236e3722b3fc
Vinicius Mantovani <viniciusmantovani@Viniciuss-MacBook-Pro.local>
2020-10-26 20:06:35 -0300

    atualizando o site da loja - modificações nos links de Produto/Cursos/Sobre

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function iconDoce() {
+    document.getElementById("icondoce").src = '/icons/icondoce.png';
+  }
+  function iconDoceCor() {
+    document.getElementById("icondoce").src = '/icons/icondocecor.png';
+  }
+
+  function iconBalao() {
+    document.getElementById("iconbalao").src = '/icons/iconbalao.png';
+  }
+  function iconBalaoCor(){
+    document.getElementById("iconbalao").src = '/icons/iconbalaocor.png';
+  }
+
+  function iconBolo() {
+    document.getElementById("iconbolo").src = '/icons/iconbolo.png';
+  }
+  function iconBoloCor(){
+    document.getElementById("iconbolo").src = '/icons/iconbolocor.png';
+  }
+
+
+  function iconDecor() {
+    document.getElementById("icondecor").src = '/icons/icondecor.png';
+  }
+  function iconDecorCor(){
+    document.getElementById("icondecor").src = '/icons/icondecorcor.png';
+  }
