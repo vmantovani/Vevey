3152168c47ee12876f8db26df75c0c5802814ef9
Vinicius Mantovani <viniciusmantovani@Viniciuss-MacBook-Pro.local>
2021-02-21 08:29:28 -0300

    Teste do Github Pages (imagens)

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 
 
 function iconDoce() {
-    document.getElementById("icondoce").src = './icons/icondoce.png';
+    document.getElementById("icondoce").src = '../icons/icondoce.png';
   }
   function iconDoceCor() {
-    document.getElementById("icondoce").src = './icons/icondocecor.png';
+    document.getElementById("icondoce").src = '../icons/icondocecor.png';
   }
 
   function iconBalao() {
-    document.getElementById("iconbalao").src = './icons/iconbalao.png';
+    document.getElementById("iconbalao").src = '../icons/iconbalao.png';
   }
   function iconBalaoCor(){
-    document.getElementById("iconbalao").src = './icons/iconbalaocor.png';
+    document.getElementById("iconbalao").src = '../icons/iconbalaocor.png';
   }
 
   function iconBolo() {
-    document.getElementById("iconbolo").src = './icons/iconbolo.png';
+    document.getElementById("iconbolo").src = '../icons/iconbolo.png';
   }
   function iconBoloCor(){
-    document.getElementById("iconbolo").src = './icons/iconbolocor.png';
+    document.getElementById("iconbolo").src = '../icons/iconbolocor.png';
   }
 
 
   function iconDecor() {
-    document.getElementById("icondecor").src = './icons/icondecor.png';
+    document.getElementById("icondecor").src = '../icons/icondecor.png';
   }
   function iconDecorCor(){
-    document.getElementById("icondecor").src = './icons/icondecorcor.png';
+    document.getElementById("icondecor").src = '../icons/icondecorcor.png';
   }
